2a9557f169832caa527db42517278b0be4a9d0cf
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2023-07-20 22:45:14 +0100

    Added Bootstrap and axios
    Added Bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
+      <div className= "container">
           Weather
+         <footer><a href= "#0">This app is coded by Kemi Oluwasanmi </a></footer>
+    </div>
     </div>
   );
 }
